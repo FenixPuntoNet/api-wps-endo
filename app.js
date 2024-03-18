2c415bce1947d1d96bfb13d642ec94cefd36798c
SamuelNarvaezFenix <liderdesarrollofenixpuntonet@gmail.com>
2024-03-18 17:40:49 -0500

    fix

--- app.js
+++ app.js
@@ -20,12 +20,9 @@
 
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
-const puppeteer = require('puppeteer');
 
 const client = new Client();
 
-// Especifica la ruta al ejecutable de Chromium
-const chromiumPath = '/bin/chromium-browser';
 
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
@@ -37,9 +34,4 @@ client.on('ready', () => {
 
 client.on("message", async (message) => {
     await client.sendMessage("573192114380@c.us", "Text");
-});
-
-// Configura Puppeteer para utilizar la ruta especificada para Chromium
-puppeteer.launch({ executablePath: chromiumPath }).then(() => {
-    client.initialize();
 });
